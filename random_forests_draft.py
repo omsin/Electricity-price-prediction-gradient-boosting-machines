3bd7b929460042f332ec74e2d46fd2818f5d6f3a
omsin <omsin4c@gmail.com>
2021-07-18 04:52:51 +0100

    First try!!

---
+++ random_forests_draft.py
@@ -0,0 +1,173 @@
+import pandas as pd
+import numpy as np
+from IPython.display import display
+from fastai.imports import *
+from sklearn import metrics
+from pandas.tseries.offsets import DateOffset
+import matplotlib.pyplot as plt
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+
+hour_ahead = 25
+N_tree = 100
+
+path = "C:/Users/omsin/PycharmProjects/pythonProject/Checkpoint in data preparation pipeline/09 Merge all.csv"
+Merge_all = pd.read_csv(path)
+dti = pd.to_datetime(Merge_all['DateTime'], format='%Y-%m-%d %H', exact=False)
+Merge_all.set_index(pd.Index(dti), inplace=True)
+Merge_all.drop(['DateTime'], axis=1, inplace=True)
+
+First = Merge_all.iloc[0:1000, :]
+N_index = First.shape
+Y_temp = First['FI']
+Y = Y_temp.iloc[hour_ahead:N_index[0]]
+Y = Y.to_numpy()
+#Y.reset_index(drop=True, inplace=True)
+#Y.columns = [''] * len(Y.columns)
+
+
+X_temp1 = First['FI']
+X_temp2 = First.loc[:, First.columns != 'FI']
+X_temp2_index = X_temp2.index + DateOffset(hours=-hour_ahead)
+X_temp2.set_index(X_temp2_index, inplace=True)
+X_temp1 = X_temp1.to_frame()
+X_temp = X_temp1.join(X_temp2)
+X = X_temp.iloc[0:N_index[0] - hour_ahead]
+X = X.to_numpy()
+#X.reset_index(drop=True, inplace=True)
+#X.columns = [''] * len(X.columns)
+Pred_index = X.shape
+
+
+
+class RandomForest():
+    def __init__(self, x, y, n_trees, n_features, sample_sz, depth=10, min_leaf=10):
+        np.random.seed(12)
+        if n_features == 'sqrt':
+            self.n_features = int(np.sqrt(x.shape[1]))
+        elif n_features == 'log2':
+            self.n_features = int(np.log2(x.shape[1]))
+        else:
+            self.n_features = n_features
+        print(self.n_features, "sha: ", x.shape[1])
+        self.x, self.y, self.sample_sz, self.depth, self.min_leaf = x, y, sample_sz, depth, min_leaf
+        self.trees = [self.create_tree() for i in range(n_trees)]
+
+    def create_tree(self):
+        idxs = np.random.permutation(len(self.y))[:self.sample_sz]
+        f_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        return DecisionTree(self.x[idxs], self.y[idxs], self.n_features, f_idxs,
+                            idxs=np.array(range(self.sample_sz)), depth=self.depth, min_leaf=self.min_leaf)
+
+    def predict(self, x):
+        return np.mean([t.predict(x) for t in self.trees], axis=0)
+
+
+def std_agg(cnt, s1, s2):
+    try:
+        return math.sqrt((s2 / cnt) - (s1 / cnt) ** 2)
+    except:
+        return 0
+
+
+class DecisionTree():
+    def __init__(self, x, y, n_features, f_idxs, idxs, depth, min_leaf):
+        self.x, self.y, self.idxs, self.min_leaf, self.f_idxs = x, y, idxs, min_leaf, f_idxs
+        self.depth = depth
+        print(f_idxs)
+        #         print(self.depth)
+        self.n_features = n_features
+        self.n, self.c = len(idxs), x.shape[1]
+        self.val = np.mean(y[idxs])
+        self.score = float('inf')
+        self.find_varsplit()
+
+    def find_varsplit(self):
+        for i in self.f_idxs: self.find_better_split(i)
+        if self.is_leaf: return
+        x = self.split_col
+        lhs = np.nonzero(x <= self.split)[0]
+        rhs = np.nonzero(x > self.split)[0]
+        lf_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        rf_idxs = np.random.permutation(self.x.shape[1])[:self.n_features]
+        self.lhs = DecisionTree(self.x, self.y, self.n_features, lf_idxs, self.idxs[lhs], depth=self.depth - 1,
+                                min_leaf=self.min_leaf)
+        self.rhs = DecisionTree(self.x, self.y, self.n_features, rf_idxs, self.idxs[rhs], depth=self.depth - 1,
+                                min_leaf=self.min_leaf)
+
+    def find_better_split(self, var_idx):
+        x, y = self.x[self.idxs, var_idx], self.y[self.idxs]
+        sort_idx = np.argsort(x)
+        sort_y, sort_x = y[sort_idx], x[sort_idx]
+        rhs_sum2: object
+        rhs_cnt, rhs_sum, rhs_sum2 = self.n, sort_y.sum(), (sort_y ** 2).sum()
+        lhs_cnt, lhs_sum, lhs_sum2 = 0, 0., 0.
+
+        for i in range(0, self.n - self.min_leaf - 1):
+            xi, yi = sort_x[i], sort_y[i]
+            lhs_cnt += 1
+            rhs_cnt -= 1
+            lhs_sum += yi
+            rhs_sum -= yi
+            lhs_sum2 += yi ** 2
+            rhs_sum2 -= yi ** 2
+            if i < self.min_leaf or xi == sort_x[i + 1]:
+                continue
+
+            lhs_std = std_agg(lhs_cnt, lhs_sum, lhs_sum2)
+            rhs_std = std_agg(rhs_cnt, rhs_sum, rhs_sum2)
+            curr_score = lhs_std * lhs_cnt + rhs_std * rhs_cnt
+            if curr_score < self.score:
+                self.var_idx, self.score, self.split = var_idx, curr_score, xi
+
+    @property
+    def split_name(self):
+        return self.x.columns[self.var_idx]
+
+    @property
+    def split_col(self):
+        return self.x[self.idxs, self.var_idx]
+
+    @property
+    def is_leaf(self):
+        return self.score == float('inf') or self.depth <= 0
+
+    def predict(self, x):
+        return np.array([self.predict_row(xi) for xi in x])
+
+    def predict_row(self, xi):
+        if self.is_leaf: return self.val
+        t = self.lhs if xi[self.var_idx] <= self.split else self.rhs
+        return t.predict_row(xi)
+
+
+forests = RandomForest(X, Y, N_tree, Pred_index[1], Pred_index[0])
+Pred = forests.predict(X)
+print(Pred)
+print(Pred.shape)
+MSE = mean_squared_error(Y, Pred, squared=True)
+print(MSE)
+RMSE = np.sqrt(MSE)
+print(RMSE)
+MAE = mean_absolute_error(Y, Pred)
+print(MAE)
+
+"""
+plt.figure(figsize=(10, 6), dpi=80)
+plt.suptitle('Random Forests, N=' + str(Pred_index[0]) + ', N_tree=' + str(N_tree) + ', depth=' + str(3) + ', min_leaf=' + str(5))
+plt.plot(np.arange(len(Y))+1, Y, color="blue", linewidth=2.5, linestyle="-")
+plt.plot(np.arange(len(Pred))+1, Pred, color="red",  linewidth=2.5, linestyle="-")
+plt.show()
+"""
+
+plt.figure(figsize=(10, 6), dpi=80)
+plt.suptitle('Random Forests, N=' + str(Pred_index[0]) + ', N_tree=' + str(N_tree) + ', depth=' + str(10) + ', min_leaf=' + str(10))
+plt.plot(np.arange(len(Y[Pred_index[0]-500:Pred_index[0]]))+1, Y[Pred_index[0]-500:Pred_index[0]], color="blue", linewidth=2.5, linestyle="-", label='Real Price')
+plt.plot(np.arange(len(Pred[Pred_index[0]-500:Pred_index[0]]))+1, Pred[Pred_index[0]-500:Pred_index[0]], color="red",  linewidth=2.5, linestyle="-", label='Predicted Price')
+plt.xlabel('Hours')
+plt.ylabel('Price(EUR)')
+plt.legend(frameon=False)
+plt.show()
